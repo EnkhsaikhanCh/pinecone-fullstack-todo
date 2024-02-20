0ad034f9c8809fbb692fb11388fe872ec69a7c96
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-02-20 21:06:15 +0800

    New components - Add Task

--- todo-ui/components/Card.jsx
+++ todo-ui/components/Card.jsx
@@ -1,5 +1,6 @@
 import { data } from "autoprefixer";
 import { useEffect, useState } from "react";
+import { AddTask } from "./AddTask";
 
 export function Card() {
   const [articles, setArticles] = useState([]);
@@ -13,30 +14,7 @@ export function Card() {
   return (
     <div className="container mx-auto my-4 flex justify-center">
       <div className="flex flex-col gap-4">
-        <div>
-          <button
-            className="btn btn-neutral"
-            onClick={() => document.getElementById("my_modal_2").showModal()}
-          >
-            Add Task
-          </button>
-          <dialog id="my_modal_2" className="modal">
-            <div className="modal-box">
-              <h3 className="text-lg font-bold">Hello!</h3>
-              <p className="py-4">Press ESC key or click outside to close</p>
-              <div className="modal-action">
-                <form method="dialog" className="flex gap-2">
-                  {/* if there is a button in form, it will close the modal */}
-                  <button className="btn btn-neutral">Close</button>
-                  <button className="btn btn-primary">Add Task</button>
-                </form>
-              </div>
-            </div>
-            <form method="dialog" className="modal-backdrop">
-              <button>close</button>
-            </form>
-          </dialog>
-        </div>
+        <AddTask />
         {articles.map((article) => (
           <div
             key={article.id}
