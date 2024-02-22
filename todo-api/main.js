d160f5870c9c6fcb7a98421eda84b9e4a11e0810
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-02-22 18:10:31 +0800

    Added function: Edit(Update) task

--- todo-api/main.js
+++ todo-api/main.js
@@ -9,12 +9,17 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-// Create
+function readTasks() {
+  const data = fs.readFileSync("tasks.json", "utf8");
+  const list = JSON.parse(data);
+  return list;
+}
+
+// Create ---------------------------------------------
 app.post("/tasks/create", (req, res) => {
   const { title } = req.body;
 
-  const data = fs.readFileSync("tasks.json", "utf8");
-  const list = JSON.parse(data);
+  const list = readTasks();
 
   const taskID = Date.now();
 
@@ -27,26 +32,34 @@ app.post("/tasks/create", (req, res) => {
   res.json([{ status: "Created" }]);
 });
 
-// Read
+// Read ---------------------------------------------
 app.get("/tasks", (req, res) => {
   const data = fs.readFileSync("tasks.json", "utf8");
   res.json(JSON.parse(data));
 });
 
-// Update
-app.put("/tasks/update/:id", (req, res) => {});
+// Update ---------------------------------------------
+app.put("/tasks/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
 
-// Delete
+  const list = readTasks();
+
+  const index = list.findIndex((item) => item.id === Number(id));
+
+  list[index].title = title;
+
+  fs.writeFileSync("tasks.json", JSON.stringify(list));
+  res.json([{ status: "Updated" }]);
+});
+
+// Delete ---------------------------------------------
 app.delete("/tasks/delete/:id", (req, res) => {
   const { id } = req.params;
-  console.log(req.params);
 
-  const data = fs.readFileSync("tasks.json", "utf8");
-  const list = JSON.parse(data);
+  const list = readTasks();
 
   const newList = list.filter((item) => item.id !== Number(id));
-
-  console.log({ list, newList });
 
   fs.writeFileSync("tasks.json", JSON.stringify(newList));
   res.json([{ status: "Deleted" }]);
--- todo-ui/components/Card.jsx
+++ todo-ui/components/Card.jsx
@@ -4,41 +4,56 @@ import { useEffect, useState } from "react";
 export function Card() {
   const [tasks, setTasks] = useState([]);
 
+  const api = `http://localhost:3000/tasks`;
+  const create = `create`;
+  const uptade = `update`;
+  const deleteParams = `delete`;
+
   function loadTasks() {
-    axios.get("http://localhost:3000/tasks").then((response) => {
+    axios.get(`${api}`).then((response) => {
       setTasks(response.data);
     });
   }
 
+  // Create ---------------------------------------------
+  function createNewTask() {
+    const title = prompt("task name?");
+
+    if (title) {
+      axios
+        .post(`${api}/${create}`, {
+          title,
+        })
+        .then(() => {
+          loadTasks();
+        });
+    }
+  }
+
+  // Read ---------------------------------------------
   useEffect(() => {
     loadTasks();
   }, []);
 
-  function createNewTask() {
-    const title = prompt("task name?");
+  // Update ---------------------------------------------
+  function editTask(task) {
+    const editedTask = prompt("Edit?", task.title);
 
-    if (!title) {
-      alert("fill title!");
-      return;
+    if (editedTask) {
+      axios
+        .put(`${api}/${uptade}/${task.id}`, {
+          title: editedTask,
+        })
+        .then(() => {
+          loadTasks();
+        });
     }
-
-    axios
-      .post("http://localhost:3000/tasks/create", {
-        title,
-      })
-      .then(() => {
-        loadTasks();
-      });
   }
 
-  function editTask() {
-    const name = prompt("Task name?");
-    console.log(name);
-  }
-
+  // Delete ---------------------------------------------
   function deleteTask(id) {
     if (confirm("Delete?")) {
-      axios.delete(`http://localhost:3000/tasks/delete/${id}`).then(() => {
+      axios.delete(`${api}/${deleteParams}/${id}`).then(() => {
         loadTasks();
       });
     }
@@ -46,34 +61,39 @@ export function Card() {
 
   return (
     <div className="container mx-auto my-4 flex justify-center">
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col">
         <div>
           <button
-            className="btn btn-primary mb-2 text-lg"
+            className="btn btn-primary mb-4 text-lg"
             onClick={createNewTask}
           >
             New task
           </button>
         </div>
-        {tasks.map((task) => (
-          <div
-            key={task.id}
-            className="item-center card flex h-[70px] w-[600px] justify-center rounded-md border border-slate-700 shadow"
-          >
-            <div className="mx-8 flex items-center gap-2 text-xl">
-              <div className="flex-1 text-white">{task.title}</div>
-              <button className="btn btn-sm" onClick={() => editTask(task.id)}>
-                edit
-              </button>
-              <button
-                className="btn btn-sm"
-                onClick={() => deleteTask(task.id)}
-              >
-                delete
-              </button>
+        <div className="flex flex-col gap-1">
+          {tasks.map((task) => (
+            <div
+              key={task.id}
+              className="item-center card flex h-[70px] w-[600px] justify-center rounded-md bg-base-200 shadow-sm"
+            >
+              <div className="mx-8 flex items-center gap-2 text-xl">
+                <div className="flex-1 text-white">{task.title}</div>
+                <button
+                  className="btn btn-neutral btn-sm"
+                  onClick={() => editTask(task)}
+                >
+                  edit
+                </button>
+                <button
+                  className="btn btn-neutral btn-sm"
+                  onClick={() => deleteTask(task.id)}
+                >
+                  delete
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
